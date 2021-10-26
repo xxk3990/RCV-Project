88fe15aaaec82f3ab5a97dbb5e7bce862b2c9770
Xander Kaylan <xxk3990@rit.edu>
2021-10-25 21:05:23 -0400

    deleted unused pages

--- jackscript.js
+++
@@ -1,9 +0,0 @@
-const candidate = document.querySelector("#nameParty");
-
-function enableMove() {
-    candidate.style.width = "600px";
-}
-
-candidate.addEventListener("click", enableMove());
-
-candidate.innerHTML = '<p>helllloooooooooooooooooooooooooooo</p>';
